1df09bdb9afdc07f1cd8b62898782037c3d7f2ab
Igor Hey Matos <iheymatos1@gmail.com>
2024-03-14 09:51:04 -0300

    Exercício12

---
+++ Exercicio12.java
@@ -0,0 +1,47 @@
+public class Exercicio12 {
+    public static void executar() {
+        int mes = Prompt.lerInteiro("Digite um mês: ");
+
+        switch (mes) {
+            case 1:
+                Prompt.imprimir("Janeiro.");
+                break;
+            case 2:
+                Prompt.imprimir("Fevereiro.");
+                break;
+            case 3:
+                Prompt.imprimir("Março.");
+                break;
+            case 4:
+                Prompt.imprimir("Abril.");
+                break;
+            case 5:
+                Prompt.imprimir("Maio.");
+                break;
+            case 6:
+                Prompt.imprimir("Junho.");
+                break;
+            case 7:
+                Prompt.imprimir("Julho.");
+                break;
+            case 8:
+                Prompt.imprimir("Agosto.");
+                break;
+            case 9:
+                Prompt.imprimir("Setembro.");
+                break;
+            case 10:
+                Prompt.imprimir("Outubro.");
+                break;
+            case 11:
+                Prompt.imprimir("Novembro.");
+                break;
+            case 12:
+                Prompt.imprimir("Dezembro.");
+                break;
+            default:
+                Prompt.imprimir("Número inválido");
+                break;
+        }
+    }
+}
